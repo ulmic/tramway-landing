9b3c9fcaffe197587b7815727570858fee0d10fd
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2018-02-21 02:06:15 +0400

    Update layout for another pages

--- app/uploaders/tramway/landing/photo_versions.rb
+++ app/uploaders/tramway/landing/photo_versions.rb
@@ -5,6 +5,10 @@ module Tramway::Landing::PhotoVersions
     version :card do
       process resize_to_fill: [400, 400, 'North']
     end
+
+    version :horizontal do
+      process resize_to_fill: [1600, 700, 'Center']
+    end
   end
 end
 
